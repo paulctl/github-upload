59657e39e8bbf300b27f9ff19608ad62c0868733
paulctl <paul.lay@centurylink.com>
2020-12-09 13:12:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res)=>{
 console.log('JS: New connection')
-res.end('PORT - Hello response  *****************************')
+res.end('Hello response  : After Auto Build/deploy')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('JS: Listening'))
